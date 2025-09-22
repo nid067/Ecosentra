570897e9fa259385b9d1ec2a71d8092d54626364
Nidhin <nidhin.240094@aiml.ritchennai.edu.in>
2025-09-22 19:41:39 +0530

    Update firebase-client.js

--- firebase-client.js
+++ firebase-client.js
@@ -1,17 +1,16 @@
 // firebase-client.js
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "api_key",
-  authDomain: "auth_domain",
-  projectId: "project_id",
-  storageBucket: "storage_bucket",
-  messagingSenderId: "messaging_senderid",
-  appId: "app_id",
-  measurementId: "measurement_id"
+  apiKey: "AIzaSyCj6rlD2XfxchBNA7QFws9bXTrMTQEYZTc",
+  authDomain: "ecosentra-b518d.firebaseapp.com",
+  projectId: "ecosentra-b518d",
+  storageBucket: "ecosentra-b518d.appspot.com",
+  messagingSenderId: "706806291686",
+  appId: "1:706806291686:web:a48e994e141fa02ff86f27",
+  measurementId: "G-MVB6202Z2D"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
-
